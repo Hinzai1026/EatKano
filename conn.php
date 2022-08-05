a9bce2b556eff4322fa54d491dc185d4ae25eaf1
Hinzai1026 <110540208+Hinzai1026@users.noreply.github.com>
2022-08-05 17:23:20 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -6,11 +6,3 @@ if ($link->connect_error) {
     die("Failed to connect: " . $conn->connect_error);
 }
 $ranking = "kano_rank";
-<?php
-//hinzai.sql
-$link = new mysqli('localhost','NAME','PASSWORD','kano');
-mysqli_set_charset($link, 'utf8');
-if ($link->connect_error) {
-    die("Failed to connect: " . $conn->connect_error);
-}
-$ranking = "kano_rank";
